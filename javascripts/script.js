20ee2349ddc2d46a73f8b949b27aad219183b711
Stephen Ehiabhi <Steveyyh123@gmail.com>
2022-09-04 19:25:05 +0200

    added the wait feature to enable two clients to wait until the other is connected

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,7 +1,8 @@
-// Canvas Related 
-const canvas = document.createElement('canvas');
-const context = canvas.getContext('2d');
-const socket = io("http://localhost:3000")
+// Canvas Related
+const canvas = document.createElement("canvas");
+const context = canvas.getContext("2d");
+const socket = io("http://localhost:3000");
+let isReferee = false;
 let paddleIndex = 0;
 
 let width = 500;
@@ -11,8 +12,8 @@ let height = 700;
 let paddleHeight = 10;
 let paddleWidth = 50;
 let paddleDiff = 25;
-let paddleX = [ 225, 225 ];
-let trajectoryX = [ 0, 0 ];
+let paddleX = [225, 225];
+let trajectoryX = [0, 0];
 let playerMoved = false;
 
 // Ball
@@ -26,11 +27,11 @@ let speedY = 2;
 let speedX = 0;
 
 // Score for Both Players
-let score = [ 0, 0 ];
+let score = [0, 0];
 
 // Create Canvas Element
 function createCanvas() {
-  canvas.id = 'canvas';
+  canvas.id = "canvas";
   canvas.width = width;
   canvas.height = height;
   document.body.appendChild(canvas);
@@ -40,23 +41,23 @@ function createCanvas() {
 // Wait for Opponents
 function renderIntro() {
   // Canvas Background
-  context.fillStyle = 'black';
+  context.fillStyle = "black";
   context.fillRect(0, 0, width, height);
 
   // Intro Text
-  context.fillStyle = 'white';
+  context.fillStyle = "white";
   context.font = "32px Courier New";
-  context.fillText("Waiting for opponent...", 20, (canvas.height / 2) - 30);
+  context.fillText("Waiting for opponent...", 20, canvas.height / 2 - 30);
 }
 
 // Render Everything on Canvas
 function renderCanvas() {
   // Canvas Background
-  context.fillStyle = 'black';
+  context.fillStyle = "black";
   context.fillRect(0, 0, width, height);
 
   // Paddle Color
-  context.fillStyle = 'white';
+  context.fillStyle = "white";
 
   // Bottom Paddle
   context.fillRect(paddleX[0], height - 20, paddleWidth, paddleHeight);
@@ -69,19 +70,19 @@ function renderCanvas() {
   context.setLineDash([4]);
   context.moveTo(0, 350);
   context.lineTo(500, 350);
-  context.strokeStyle = 'grey';
+  context.strokeStyle = "grey";
   context.stroke();
 
   // Ball
   context.beginPath();
   context.arc(ballX, ballY, ballRadius, 2 * Math.PI, false);
-  context.fillStyle = 'white';
+  context.fillStyle = "white";
   context.fill();
 
   // Score
   context.font = "32px Courier New";
-  context.fillText(score[0], 20, (canvas.height / 2) + 50);
-  context.fillText(score[1], 20, (canvas.height / 2) - 30);
+  context.fillText(score[0], 20, canvas.height / 2 + 50);
+  context.fillText(score[1], 20, canvas.height / 2 - 30);
 }
 
 // Reset Ball to Center
@@ -160,32 +161,41 @@ function animate() {
   window.requestAnimationFrame(animate);
 }
 
-// Start Game, Reset Everything
-function startGame() {
+// Load Game, Reset Everything
+function loadGame() {
   createCanvas();
   renderIntro();
+  socket.emit("ready");
+}
 
-  //socket.emit("ready", {socket.id})
-  
-  paddleIndex = 0;
+function startGame() {
+  paddleIndex = isReferee ? 0 : 1;
   window.requestAnimationFrame(animate);
-  canvas.addEventListener('mousemove', (e) => {
+  canvas.addEventListener("mousemove", (e) => {
     playerMoved = true;
     paddleX[paddleIndex] = e.offsetX;
     if (paddleX[paddleIndex] < 0) {
       paddleX[paddleIndex] = 0;
     }
-    if (paddleX[paddleIndex] > (width - paddleWidth)) {
+    if (paddleX[paddleIndex] > width - paddleWidth) {
       paddleX[paddleIndex] = width - paddleWidth;
     }
     // Hide Cursor
-    canvas.style.cursor = 'none';
+    canvas.style.cursor = "none";
   });
 }
 
 // On Load
-startGame();
+loadGame();
 
-socket.on('connect', () => {
-  console.log("Connected as ...", socket.id)
-})
+socket.on("connect", () => {
+  console.log("Connected as ...", socket.id);
+});
+
+socket.on("startGame", (refereeID) => {
+  console.log("Referee is", refereeID);
+
+  isReferee = socket.id === refereeID;
+  startGame()
+
+});
--- server.js
+++ server.js
@@ -13,9 +13,26 @@ server.listen(PORT);
 
 console.log(`Listening on PORT ${PORT}`);
 
+let readyPlayerCount = 0;
+
 io.on("connection", (socket) => {
+  //check when a user is connected
   console.log("a user connected", socket.id);
 
- // socket.broadcast.emit('startGame', isReferee)
+  //listening for the ready event from the client
+  socket.on("ready", () => {
+    console.log("Player ready", socket.id);
 
+    readyPlayerCount++;
+
+    if (readyPlayerCount === 2) {
+      //Broadcast startGame event to both players
+      //choosing the last player as the referee
+      io.emit("startGame", socket.id);
+    }
+  });
+
+  socket.on("disconnect", (socket) => {
+    console.log("User with id ... ", socket.id, " disconnected");
+  });
 });
